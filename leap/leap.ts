6177bb6406228f4d9fc07463c798c2db12f62c11
courtney l <clandau@learnics.com>
2020-09-12 15:19:33 -0400

    leap submitted solution all tests passing

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,5 +1,19 @@
-function isLeapYear( /* Parameters go here */ ) {
-    // Your code here
+function isLeapYear( year : number ): boolean {
+   /**
+    * Given a year, report if it is a leap year.
+
+    The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+    on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+        unless the year is also evenly divisible by 400
+    For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+    */
+    if (year % 4 !== 0) return false;
+    if (year % 100 === 0) {
+        return year % 400 === 0 ? true : false; 
+    }
+    return true;
 }
 
 export default isLeapYear
